c43b749c2ca7927cf0c982b773c8b58442a2a904
ParadigmMango <jacobjhartt@gmail.com>
2025-11-17 20:11:45 -0700

    fixed activation timespent tracking

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,18 @@
 console.log("hi from worker");
 let startTime = null;
+let curTab = null;
 
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
 	const tab = await chrome.tabs.get(activeInfo.tabId);
 	console.log(tab.url);
 
-	const timeSpent = Date.now() - startTime;
+	if (curTab) {
+		const timeSpent = Date.now() - startTime;
+		console.log(timeSpent);
+	}
+
+	curTab = tab;
 	startTime = Date.now();
+
 	console.log(startTime);
-	console.log(timeSpent);
 });
