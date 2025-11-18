d22b97cd8c6c23f037dcdca42269566a8b93a90d
ParadigmMango <jacobjhartt@gmail.com>
2025-11-17 19:57:47 -0700

    added basic timespent fetching

--- scripts/background.js
+++ scripts/background.js
@@ -1,1 +1,12 @@
 console.log("hi from worker");
+let startTime = null;
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+	const tab = await chrome.tabs.get(activeInfo.tabId);
+	console.log(tab.url);
+
+	const timeSpent = Date.now() - startTime;
+	startTime = Date.now();
+	console.log(startTime);
+	console.log(timeSpent);
+});
