c166c2159556f3d858526bb06ef0499b2645ae03
ParadigmMango <jacobjhartt@gmail.com>
2025-11-18 20:06:56 -0700

    added audible tab change tracking

--- scripts/background.js
+++ scripts/background.js
@@ -8,24 +8,36 @@ let audibleTabs = null;
 
 
 async function updateAudibleTabs() {
-	audibleTabs = (await chrome.tabs.query({ audible: true })).map(tab => ({"id": tab.id, "url": tab.url}));
+	audibleTabs = (await chrome.tabs.query({ audible: true })).map(tab => ({"id": tab.id, "url": tab.url, "windowId": tab.windowId }));
 	console.log(audibleTabs);
 }
 updateAudibleTabs();
 
 
+async function trackTab() {
+	const activeTab = (await chrome.tabs.query({ active: true, lastFocusedWindow: true })).map(tab => ({"id": tab.id, "url": tab.url, "windowId": tab.windowId }));
+
+	console.log(activeTab);
+}
+trackTab();
+
+
 // track window focus
-browser.windows.onFocusChanged.addListener((windowId) => {
+chrome.windows.onFocusChanged.addListener((windowId) => {
 	curWindow = windowId;
 
-	console.log(curWindow);
+	console.log("Cur window:" + curWindow);
 });
 
 
 // track tab activation
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
+	console.log("Tab activated");
+
 	const tab = await chrome.tabs.get(activeInfo.tabId);
 	console.log(tab.url);
+
+	trackTab();
 
 	if (curTab) {
 		const timeSpent = Date.now() - startTime;
@@ -37,3 +49,16 @@ chrome.tabs.onActivated.addListener(async (activeInfo) => {
 
 	console.log(startTime);
 });
+
+function logUpdated(tabId, changeInfo, tab) {
+	console.log("Tab updated");
+	console.log(tabId);
+	console.log(changeInfo);
+	console.log(tab);
+}
+
+const audibleFilter = {
+	properties: ["audible"]
+};
+
+chrome.tabs.onUpdated.addListener(logUpdated, audibleFilter);
